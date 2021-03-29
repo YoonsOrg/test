426d3550c746cc2189db17b84e24f835715bd620
KaneYoon <37324166+ColorkrewYoonj@users.noreply.github.com>
2021-03-29 19:08:02 +0900

    Update production.sh

--- .github/workflows/production.sh
+++ .github/workflows/production.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "envname=prod" >> $GITHUB_ENV
 
-if [ $1 = "release"]; then
+if [ $1 = release]; then
   echo "tagname=${ branchName }" >> $GITHUB_ENV
 else
   echo "tagname=${{ github.sha }}" >> $GITHUB_ENV
