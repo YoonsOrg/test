433a501789d4bae7c7d967eb2d66b870e4fe8ca2
KaneYoon <37324166+ColorkrewYoonj@users.noreply.github.com>
2021-03-29 18:56:40 +0900

    Update main.sh

--- .github/workflows/main.sh
+++ .github/workflows/main.sh
@@ -1,9 +1,3 @@
 
 #!/bin/bash
 echo "envname=staging" >> $GITHUB_ENV
-
-if [ $1 = "release"]; then
-  echo "tagname=${ branchName }" >> $GITHUB_ENV
-else
-  echo "tagname=${{ github.sha }}" >> $GITHUB_ENV
-fi
