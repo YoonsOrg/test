451dcd3d2172f499f6da9d989804d1acd325c11b
KaneYoon <37324166+ColorkrewYoonj@users.noreply.github.com>
2021-03-31 09:15:28 +0900

    Update main.sh

--- .github/workflows/main.sh
+++ .github/workflows/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "envname=prod" >> $GITHUB_ENV
-echo "secrets=$(echo ${{ env.A }})" >> $GITHUB_ENV
+echo "secrets=${{ env.A }}" >> $GITHUB_ENV
 
 if [ "$1" = "release" ]; then
   echo "tagname=$2" >> $GITHUB_ENV
